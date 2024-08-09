15a08dd98eefe6c22af9ea37319ab521b84c6a0a
ruozzy66 <shatilov.aus@gmail.com>
2024-08-09 20:31:27 +1000

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/api/chat', (req, res) => {
     pythonPath: 'python3',
     pythonOptions: ['-u'],
     scriptPath: __dirname,
-    args: [process.env.PINECONE_API_KEY, 'ti4-rules-assistant', messages],
+    args: [process.env.PINECONE_API_KEY, 'ti4-late', messages],
     timeout: 30 * 1000 // 30 seconds timeout
   };
 
